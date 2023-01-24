7a9cd0845ecc5d7e67b9e6fcb3ef55c02c1b8d09
Ana Luiza Favacho <analufavacho@gmail.com>
2023-01-24 10:52:14 -0300

    adicionando login required

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
 router.post('/create/', userController.create);
-router.get('/', userController.index);
-router.get('/:id', userController.show);
-router.put('/:id', userController.update);
-router.delete('/:id', userController.delete);
+router.get('/', loginRequired, userController.index);
+router.get('/:id', loginRequired, userController.show);
+router.put('/:id', loginRequired, userController.update);
+router.delete('/:id', loginRequired, userController.delete);
 
 export default router;
 
